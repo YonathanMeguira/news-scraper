de74ff43712ca10c7c2c677ca0e39dc3c61541d0
jonathanmeguira <johnmeguira@gmail.com>
2017-07-10 10:52:17 +0300

    added the news scrapper methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var news_scrapper = require('./lib/cyber-news-scrapper');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,14 +8,17 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
-app.get('/news', function(request, response){
-  response.send('coucou')
-})
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+app.get('/news', function (request, response) {
+    news_scrapper.getNews().then((news) => {
+        response.send(news)
+    })
+});
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
