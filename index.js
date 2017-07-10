47080b3e74b6b3198c02061e07bf4133328846ca
jonathanmeguira <johnmeguira@gmail.com>
2017-07-10 10:45:01 +0300

    changed dir

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/db');
 });
 app.get('/news', function(request, response){
   response.send('coucou')
